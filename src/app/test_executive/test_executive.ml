47ba18ba6ed343359438fcc9c0b3940cba103c21
Quantifier <quantifier-tech@protonmail.com>
2023-09-03 00:00:18 +0000

    (test exec) Cleanup after network setup in mock test

--- src/app/test_executive/test_executive.ml
+++ src/app/test_executive/test_executive.ml
@@ -400,44 +400,50 @@ let main inputs =
         in
         [%log trace] "initializing network abstraction" ;
         let%bind () = Engine.Network.initialize_infra ~logger network in
-        [%log info] "starting the daemons within the pods" ;
-        let start_print (node : Engine.Network.Node.t) =
-          let open Malleable_error.Let_syntax in
-          [%log info] "starting %s..." (Engine.Network.Node.id node) ;
-          let%bind res = Engine.Network.Node.start ~fresh_state:false node in
-          [%log info] "%s started" (Engine.Network.Node.id node) ;
-          Malleable_error.return res
-        in
-        let seed_nodes =
-          network |> Engine.Network.seeds |> Core.String.Map.data
-        in
-        let non_seed_pods =
-          network |> Engine.Network.all_non_seed_pods |> Core.String.Map.data
-        in
-        let _offline_node_event_subscription =
-          (* Monitor for offline nodes; abort the test if a node goes down
-             unexpectedly.
-          *)
-          Dsl.Event_router.on (Dsl.event_router dsl) Node_offline
-            ~f:(fun offline_node () ->
-              let node_name = Engine.Network.Node.app_id offline_node in
-              [%log info] "Detected node offline $node"
-                ~metadata:[ ("node", `String node_name) ] ;
-              if Engine.Network.Node.should_be_running offline_node then (
-                [%log fatal] "Offline $node is required for this test"
+        if String.equal test_name "mock" then (
+          [%log info] "No node interactions in mock network" ;
+          return () )
+        else (
+          [%log info] "starting the daemons within the pods" ;
+          let start_print (node : Engine.Network.Node.t) =
+            let open Malleable_error.Let_syntax in
+            [%log info] "starting %s..." (Engine.Network.Node.id node) ;
+            let%bind res = Engine.Network.Node.start ~fresh_state:false node in
+            [%log info] "%s started" (Engine.Network.Node.id node) ;
+            Malleable_error.return res
+          in
+          let seed_nodes =
+            network |> Engine.Network.seeds |> Core.String.Map.data
+          in
+          let non_seed_pods =
+            network |> Engine.Network.all_non_seed_pods |> Core.String.Map.data
+          in
+          let _offline_node_event_subscription =
+            (* Monitor for offline nodes; abort the test if a node goes down
+               unexpectedly.
+            *)
+            Dsl.Event_router.on (Dsl.event_router dsl) Node_offline
+              ~f:(fun offline_node () ->
+                let node_name = Engine.Network.Node.app_id offline_node in
+                [%log info] "Detected node offline $node"
                   ~metadata:[ ("node", `String node_name) ] ;
-                failwith "Aborted because of required offline node" ) ;
-              Async_kernel.Deferred.return `Continue )
-        in
-        (* TODO: parallelize (requires accumlative hard errors) *)
-        let%bind () = Malleable_error.List.iter seed_nodes ~f:start_print in
-        let%bind () =
-          Dsl.(wait_for dsl @@ Wait_condition.nodes_to_initialize seed_nodes)
-        in
-        let%bind () = Malleable_error.List.iter non_seed_pods ~f:start_print in
-        [%log info] "daemons started" ;
-        [%log trace] "executing test" ;
-        T.run network dsl )
+                if Engine.Network.Node.should_be_running offline_node then (
+                  [%log fatal] "Offline $node is required for this test"
+                    ~metadata:[ ("node", `String node_name) ] ;
+                  failwith "Aborted because of required offline node" ) ;
+                Async_kernel.Deferred.return `Continue )
+          in
+          (* TODO: parallelize (requires accumlative hard errors) *)
+          let%bind () = Malleable_error.List.iter seed_nodes ~f:start_print in
+          let%bind () =
+            Dsl.(wait_for dsl @@ Wait_condition.nodes_to_initialize seed_nodes)
+          in
+          let%bind () =
+            Malleable_error.List.iter non_seed_pods ~f:start_print
+          in
+          [%log info] "daemons started" ;
+          [%log trace] "executing test" ;
+          T.run network dsl ) )
   in
   let exit_reason, test_result =
     match monitor_test_result with
