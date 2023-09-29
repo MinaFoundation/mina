46b459ce5211e973dbc3dea19382f2a0a4bee842
ejMina226 <118474890+ejMina226@users.noreply.github.com>
2023-09-29 16:29:39 +0100

    Update test

--- src/app/test_executive/block_reward_test.ml
+++ src/app/test_executive/block_reward_test.ml
@@ -17,11 +17,12 @@ module Make (Inputs : Intf.Test.Inputs_intf) = struct
     { default with
       genesis_ledger =
         [ test_account "node-key" "1000"
-        ; test_account "node1-key" "1000"
-        ; test_account "node2-key" "0"
+        ; test_account "snark-node-key1" "0"
+        ; test_account "snark-node-key2" "0"
         ]
-    ; block_producers = [ bp "node" (); bp "node1" () ]
-    ; seed_nodes = [ seed "node3" (); seed "node4" () ]
+    ; block_producers = [ bp "node" () ]
+    ; snark_coordinator = snark "snark-node" ~account_name:"snark-node-key1" 4
+    ; snark_worker_fee = "0.0002"
     }
 
   let run network t =
